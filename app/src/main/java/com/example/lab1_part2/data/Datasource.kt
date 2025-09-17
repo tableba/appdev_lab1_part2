dc0d92655491068c98acce9e3f5c5883445f8651
dell <tableba@tableba.com>
2025-09-17 14:06:26 +0200

    init

---
+++ app/src/main/java/com/example/lab1_part2/data/Datasource.kt
@@ -0,0 +1,16 @@
+package com.example.lab1_part2.data
+
+import com.example.lab1_part2.R
+
+class Datasource {
+    fun loadImages(): List<Int> {
+        return listOf(
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+        )
+    }
+}
